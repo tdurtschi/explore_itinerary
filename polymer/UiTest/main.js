4e7e87b74dd5fbae96c643cb34dddb7da1c9718d
U-BOSTON\jacob.taylor <jacob.taylor@usb-lttbsjaket.ef.com>
2015-12-17 16:12:42 -0500

    figured out enumerating elements from javascript list, and binding from selected index to map latitude!

--- polymer/UiTest/main.js
+++ polymer/UiTest/main.js
@@ -2,12 +2,3 @@ var toastGroupTemplate = document.querySelector('#toastGroup');
 toastGroupTemplate.showToast = function() {
   document.querySelector('#toast').show();
 };
-Polymer({
-  is:'dom-bind',
-  properties: {
-    features: [
-      {category:'pizza',name:'Pepe\'s Pizzaria Neopoliana'},
-      {category:'pizza',name:'Salley\'s Apizza'},
-      {category:'other',name:'check this out'}]
-  }
-});
