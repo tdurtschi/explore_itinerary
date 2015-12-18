999c6438aa49f7f52407ac98279906ee54cf1f5d
ian prendergast <ihprendergast@gmail.com>
2015-12-18 09:39:14 -0500

    added maps file

---
+++ services/CollabatronMaps2015/Colabatron.DataAccess/Colabatron.DataAccess/GoogleMapsAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleMapsApi;
+using System.Web.Configuration;
+using GoogleMapsApi.Entities.Common;
+using GoogleMapsApi.Entities.Places.Request;
+using GoogleMapsApi.Entities.Places.Response;
+
+namespace Colabatron.DataAccess
+{
+    public class GoogleMapsAccess
+    {
+        public PlacesResponse GetNearbyLocations(double lat, double lon)
+        {
+            var request = new GoogleMapsApi.Entities.Places.Request.PlacesRequest()
+            {
+                ApiKey = WebConfigurationManager.AppSettings["MapsApiKey"],
+                IsSSL = true,
+                Language = "en",
+                Location = new Location(lat,lon),
+                RankBy = RankBy.Distance
+                
+            };
+            var result = GoogleMaps.Places.Query(request);
+            return result;
+        }
+    }
+}
---
+++ services/CollabatronMaps2015/CollabatronMaps2015/Controllers/NearbyPlacesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Colabatron.DataAccess;
+
+namespace CollabatronMaps2015.Controllers
+{
+    public class NearbyPlacesController : ApiController
+    {
+        public JsonResult<GoogleMapsApi.Entities.Places.Response.PlacesResponse> GetNearbyPlaces(string id)
+        {
+            return Json(new GoogleMapsAccess().GetNearbyLocations(45.56758, -73.74075));
+        }
+    }
+}
